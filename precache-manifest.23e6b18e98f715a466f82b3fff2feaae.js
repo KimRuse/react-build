b2fc5e5c8e30f4ab931bbc05467498b40682ff35
KimRuse <1327192007@qq.com>
2019-07-16 00:02:13 +0800

    1

---
+++ precache-manifest.23e6b18e98f715a466f82b3fff2feaae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a605305f3a475d039e4a71f819720376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08087a846271e91c870b",
+    "url": "/static/css/2.cdaf15d1.chunk.css"
+  },
+  {
+    "revision": "08087a846271e91c870b",
+    "url": "/static/js/2.82f30001.chunk.js"
+  },
+  {
+    "revision": "4c900b1e857ce88a9c83",
+    "url": "/static/js/main.275eb6c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8cf845355f38cc860b13f0f9425d1556",
+    "url": "/static/media/header-bg.8cf84535.png"
+  },
+  {
+    "revision": "399a7619c36aeee809a1096f2a29a100",
+    "url": "/static/media/main-bg.399a7619.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
